e3bb33f81f2371f093ef092789194cca34fd722a
BalajiStark <balajicse.2910@gmail.com>
2024-07-29 17:11:19 +0530

    Update home.component.ts
    Removed space

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 export class HomeComponent {
 
   src = "https://img.hotimg.com/pexels-donald-tong-189296.jpeg";
-
   constructor(private renderer: Renderer2) {
     this.renderer.setStyle(document.body, 'background-size', 'cover');
     this.renderer.setStyle(document.body, 'background-repeat', 'no-repeat');
